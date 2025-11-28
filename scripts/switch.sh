39c62087e9e65f54977952b420dbd6c34562c38c
Jenni-13 <2022371065@uteq.edu.mx>
2025-11-27 20:09:37 -0600

    Prueba switch

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 if [ "$1" == "blue" ]; then
-    sed -i 's/green/blue/g' nginx/default.conf
+    sed -i 's/green/blue/g' ~/Estrategia-Blue---Green/nginx/default.conf
 elif [ "$1" == "green" ]; then
-    sed -i 's/blue/green/g' nginx/default.conf
+    sed -i 's/blue/green/g' ~/Estrategia-Blue---Green/nginx/default.conf
 else
     echo "Uso correcto: ./switch.sh blue | green"
     exit 1
 fi
 
-docker-compose restart nginx
+# Reinicia el contenedor de Nginx directamente
+docker restart nginx_lb
 
 echo "🔁 Tráfico redirigido a $1 ✓"
