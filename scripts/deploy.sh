b65ea9d6c13d215ad55c0d28337312ef9ca5b14c
Jennifer Baltazar Rico <2022371065@uteq.edu.mx>
2025-11-28 14:50:55 -0600

    Create deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+cd ~/Estrategia-Blue---Green
+
+# 1. Leer el estado actual
+CURRENT_COLOR=$(grep CURRENT_DEPLOYMENT front/.env | cut -d '=' -f 2)
+
+if [ "$CURRENT_COLOR" == "blue" ]; then
+    NEW_COLOR="green"
+elif [ "$CURRENT_COLOR" == "green" ]; then
+    NEW_COLOR="blue"
+else
+    # Fallback si .env está vacío o corrupto
+    echo "ERROR: CURRENT_DEPLOYMENT no encontrado o no válido. Desplegando en BLUE por defecto."
+    NEW_COLOR="blue"
+fi
+
+echo "Desplegando la nueva versión en el entorno INACTIVO: $NEW_COLOR"
+
+# 2. Despliegue del nuevo entorno usando el script específico
+if [ "$NEW_COLOR" == "blue" ]; then
+    bash scripts/deploy-blue.sh
+else
+    bash scripts/deploy-green.sh
+fi
+
+# 3. Health Check (Nos aseguramos de que el contenedor se esté ejecutando)
+echo "Realizando Health Check: verificando que frontend_$NEW_COLOR esté UP..."
+docker ps | grep frontend_$NEW_COLOR || exit 1
+
+# 4. Switch de tráfico
+echo "Cambiando Nginx para apuntar a: $NEW_COLOR"
+bash scripts/switch.sh $NEW_COLOR
+
+# 5. Actualizar el .env
+sed -i "s/^CURRENT_DEPLOYMENT=.*/CURRENT_DEPLOYMENT=$NEW_COLOR/" front/.env
+echo "Estado de CURRENT_DEPLOYMENT actualizado a $NEW_COLOR."
+
+# 6. Reiniciar Nginx para aplicar el cambio de tráfico
+docker compose restart nginx
